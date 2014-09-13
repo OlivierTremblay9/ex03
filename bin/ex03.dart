8d522f77a663c114b2887bb427b4f0a9d67d11ee
OlivierTremblay9 <oli_0007@hotmail.com>
2014-09-12 21:59:17 -0400

    first commit

---
+++ bin/ex03.dart
@@ -0,0 +1,40 @@
+
+ main() { 
+   var a = 0; //Name Varriable
+   
+   do { 
+     a++; 
+   
+     if (a%3 == 0){    //Multiple of 3
+         print('Buzz');
+         }
+     else if (a%5 == 0){   //Multiple of 5
+         print('FizzBuzz');
+     }
+     
+     else  {        
+       print(a);
+   }
+    
+   } while (a < 100); 
+   
+
+         }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
