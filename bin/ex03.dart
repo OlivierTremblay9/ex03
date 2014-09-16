34af0c0dc6077c7c3086187b0ebfae320080c1b9
OlivierTremblay9 <oli_0007@hotmail.com>
2014-09-15 20:52:34 -0400

    update FizzBuyzz

--- bin/ex03.dart
+++ bin/ex03.dart
@@ -4,18 +4,25 @@
    
    do { 
      a++; 
-   
-     if (a%3 == 0){    //Multiple of 3
-         print('Buzz');
-         }
-     else if (a%5 == 0){   //Multiple of 5
-         print('FizzBuzz');
-     }
      
+     if ((a%3 == 0) && (a%5 == 0)){   //Multiple of 5
+       print('FizzBuzz');
+       
+       }
+     else if (a%3 == 0){    //Multiple of 3
+         print('Fizz');
+         }
+   
+                      
+     else if (a%5 == 0){   //Multiple of 5
+             print('Buzz');
+         }
+  
+   
      else  {        
        print(a);
    }
-    
+     
    } while (a < 100); 
    
 
