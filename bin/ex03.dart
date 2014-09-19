bda2f53ac0b593d5d1c8eafd77747d62a6619605
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-09-19 10:12:32 +0200

    add dr.txt

--- bin/ex03.dart
+++ bin/ex03.dart
@@ -6,27 +6,19 @@
      a++; 
      
      if ((a%3 == 0) && (a%5 == 0)){   //Multiple of 5
-       print('FizzBuzz');
-       
-       }
+       print('FizzBuzz');       
+     }
      else if (a%3 == 0){    //Multiple of 3
-         print('Fizz');
-         }
-   
-                      
+       print('Fizz');
+     }                   
      else if (a%5 == 0){   //Multiple of 5
-             print('Buzz');
-         }
-  
-   
+       print('Buzz');
+     }
      else  {        
        print(a);
-   }
-     
+     }    
    } while (a < 100); 
-   
-
-         }
+}
  
 
 
